4c598fe6468dcf7afbbc13e10e6503158316234e
Eric Li <li.eric00@gmail.com>
2016-08-19 22:17:10 -0400

    Remove Extraneous Buttons

--- js/custom.js
+++ js/custom.js
@@ -14,22 +14,8 @@
     e.preventDefault();
   });
 
-  var $button = $("<div id='source-button' class='btn btn-primary btn-xs'>&lt; &gt;</div>").click(function(){
-    var html = $(this).parent().html();
-    html = cleanSource(html);
-    $("#source-modal pre").text(html);
-    $("#source-modal").modal();
-  });
-
   $('.bs-component [data-toggle="popover"]').popover();
   $('.bs-component [data-toggle="tooltip"]').tooltip();
-
-  $(".bs-component").hover(function(){
-    $(this).append($button);
-    $button.show();
-  }, function(){
-    $button.hide();
-  });
 
   function cleanSource(html) {
     html = html.replace(/×/g, "&times;")
